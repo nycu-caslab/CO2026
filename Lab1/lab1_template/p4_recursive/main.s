f9cae63da793743e2c994775120da8a0816a22ba
yyh-603 <yiyanghung603@gmail.com>
2026-02-22 07:12:19 -0800

    fix: update lab1 p4

--- Lab1/lab1_template/p4_recursive/main.s
+++ Lab1/lab1_template/p4_recursive/main.s
@@ -4,5 +4,5 @@
 .global binary_search_asm
 
 binary_search_asm:
-    # TODO: You have to implement fibonacci with assembly language
+    # TODO: You have to implement binary search with assembly language
     # HINT: You might need to use "jal(jump and link)" to finish the task
